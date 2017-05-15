1eebe5beaa1433e4ce618509764bd2265d75fa24
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2017-05-15 20:45:31 +0100

    Chapter 15

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'foundation-rails', '~> 5.0'
 gem 'react-rails'
 gem 'flux-rails-assets'
 gem 'lodash-rails'
+gem 'sprockets-es6'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/assets/javascripts/components/comment_list.js.jsx
+++ app/assets/javascripts/components/comment_list.js.jsx
@@ -1,17 +1,17 @@
 var CommentList = React.createClass({
 
   componentDidMount: function() {
-    Store.addChangeListener(this._onChange);
+    commentStore.addChangeListener(this._onChange);
   },
 
   componentWillUnmount: function() {
-    Store.removeChangeListener(this._onChange);
+    commentStore.removeChangeListener(this._onChange);
   },
 
   render: function() {
     return (
       <div>
-        {Store.comments().map(function(comment) {
+        {commentStore.comments().map(function(comment) {
           return <Comment key={comment.id} {... comment}/>
         })}
       </div>
